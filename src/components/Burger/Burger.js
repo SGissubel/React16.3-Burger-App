ddace736408a151f5277d01b4f7c7fae21c50a50
Steven Gissubel <stevengissubel@rb-mac-007.home>
2018-04-27 11:19:06 -0400

    cur

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,8 +8,15 @@ const burger = (props) => {
 		.map( igKey => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />
-			})
-		});
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please Start Adding Ingredients</p>;
+	}
 
 	return (
 		<div className={classes.Burger}>
